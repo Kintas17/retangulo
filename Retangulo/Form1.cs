1422504a6f3409ca8587d6ecb6c87eb932fdf11a
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-11-25 18:14:13 +0000

    Add files via upload

---
+++ Retangulo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        retangulo cunha = new retangulo();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cunha.recebe = textBox1.Text;
+            label1.Text = cunha.envia;
+        }
+    }
+}
---
+++ Retangulo/retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo
+{
+    class retangulo
+    {
+        private double valor;
+        private double valor2;
+
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToDouble(value)* valor2;
+            }
+        }
+        public string envia
+        {
+            get
+            {
+               return valor.ToString();
+            }
+        }
+    }
+}
